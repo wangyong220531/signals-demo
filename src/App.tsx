a02995f17856de964337c2cd685363d60455225d
wangyong220531 <wangyong220531@gmail.com>
2023-12-09 14:20:36 +0800

    202312091325update

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,25 @@
 
-import { computed, effect,signal, untracked } from '@preact/signals-react';
+import { computed, effect, signal, untracked } from '@preact/signals-react';
 import './App.css'
 
-const name = signal("Jane");
-const surname = signal("Doe");
-const fullName = computed(() => name.value + " " + surname.value);
-function foo(){
-  return name.value = "Joe"
+const firstName = signal("Jane");
+const lastName = signal("Doe");
+const fullName = computed(() => firstName.value + " " + lastName.value);
+
+function foo() {
+  return firstName.value = "Joe"
 }
 
 effect(() => {
   console.log(fullName.value);
-  name.value = untracked(foo)
+  firstName.value = untracked(foo)
 });
 
 function App() {
-
-  
-
   return (
-    <>
-      <button onClick={foo}>
-        {fullName}
-      </button>
-    </>
+    <button onClick={foo}>
+      {fullName}
+    </button>
   )
 }
 
